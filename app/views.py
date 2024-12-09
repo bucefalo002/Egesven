8dc851c55886c481dc4179410beabda54a36626f
Juan Lattapiat <juan.lattapiat.j@gmail.com>
2024-12-09 12:25:48 -0300

    links entre las ventanas  y carga del html base

--- app/views.py
+++ app/views.py
@@ -1,4 +1,4 @@
-from django.shortcuts import render
+from django.shortcuts import render, redirect
 
 # Create your views here.
 
@@ -14,3 +14,5 @@ def carrito(request):
 
 def pago(request):
     return render(request, 'app/pago.html')
+
+
